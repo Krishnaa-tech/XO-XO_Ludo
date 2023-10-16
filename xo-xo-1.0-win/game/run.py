bfb50335dd980e9fd07a7070efff56517c67cc76
harshi1gfiesg <101106341+harshi1gfiesg@users.noreply.github.com>
2023-10-16 10:24:10 +0530

    delete unwanted files

--- xo-xo-1.0-win/game/run.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("C:/Users/Lenovo/Documents/SEM 5/Advanced Python/chutiya/lib/pygame_sdl2/src/pygame_sdl2/font.pyx")
-)
